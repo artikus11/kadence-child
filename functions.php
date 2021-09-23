93d786094b6b979f5677740085d4b189f71b89ea
artikus13 <artikus.sol@gmail.com>
2021-09-23 23:03:19 +0400

    Управление стилями через дочерку

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@ function child_enqueue_styles() {
 	wp_enqueue_style( 'child-theme', get_stylesheet_directory_uri() . '/style.css', array(), 100 );
 }
 
-// add_action( 'wp_enqueue_scripts', 'child_enqueue_styles' ); // Remove the // from the beginning of this line if you want the child theme style.css file to load on the front end of your site.
+add_action( 'wp_enqueue_scripts', 'child_enqueue_styles' ); // Remove the // from the beginning of this line if you want the child theme style.css file to load on the front end of your site.
 
 /**
  * Add custom functions here
