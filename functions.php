53e6e717df439cae5af874cf27ce381e17885d6f
artikus13 <artikus.sol@gmail.com>
2021-09-23 23:02:28 +0400

    Init

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Enqueue child styles.
+ */
+function child_enqueue_styles() {
+	wp_enqueue_style( 'child-theme', get_stylesheet_directory_uri() . '/style.css', array(), 100 );
+}
+
+// add_action( 'wp_enqueue_scripts', 'child_enqueue_styles' ); // Remove the // from the beginning of this line if you want the child theme style.css file to load on the front end of your site.
+
+/**
+ * Add custom functions here
+ */
